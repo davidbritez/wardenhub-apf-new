0cae66738ed19d77a311d899fe88d449b0f066f6
Bryan Giger <bryan@penguin.academy>
2019-12-13 03:45:17 +0100

    create general index & signup form

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, Response
-
+import face_recognition
 app = Flask(__name__, static_url_path='', static_folder='static')
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/api/add_user', methods=['POST'])
 def add_user():
